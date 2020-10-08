11f4bad7be1beb74488cb20e4bfd98d6dac33a2b
Laura <laura.allitt@hotmail.com>
2020-10-08 18:07:22 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const menu_item = document.querySelectorAll('.sidebar .menu .menu-item .menu-link');
+
+window.onload = () => {
+    setTimeout(() => {
+      document.querySelector("body").classList.add("display");
+    }, 4000);
+  };
+  
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+});
+
+document.querySelector(".scroll-btn").addEventListener ("click", () => {
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector("html").style.scrollBehavior = "unset";
+    }, 1000);
+});
+
+menu_item.forEach(item => {
+    item.addEventListener("click" , () => {
+        document.querySelector(".container").classList.toggle('change');
+        document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector("html").style.scrollBehavior = "unset";
+    }, 1000);
+    });
+});
