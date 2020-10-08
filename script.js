7c62ca881b98355c33f4c3568d2dfc8f514ebf98
Laura <laura.allitt@hotmail.com>
2020-10-08 18:24:09 +0100

    reverted original touch addition; added sepearte code for this

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ window.onload = () => {
     }, 4000);
   };
   
-document.querySelector(".hamburger-menu").addEventListener("click touch", () => {
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("change");
 });
 
-document.querySelector(".scroll-btn").addEventListener ("click touch", () => {
+document.querySelector(".scroll-btn").addEventListener ("click", () => {
     document.querySelector("html").style.scrollBehavior = "smooth";
     setTimeout(() => {
         document.querySelector("html").style.scrollBehavior = "unset";
     }, 1000);
 });
 
-document.querySelector(".button").addEventListener ("click touch", () => {
+document.querySelector(".button").addEventListener ("click", () => {
     document.querySelector("html").style.scrollBehavior = "smooth";
     setTimeout(() => {
         document.querySelector("html").style.scrollBehavior = "unset";
@@ -25,7 +25,35 @@ document.querySelector(".button").addEventListener ("click touch", () => {
 });
 
 menu_item.forEach(item => {
-    item.addEventListener("click touch" , () => {
+    item.addEventListener("click" , () => {
+        document.querySelector(".container").classList.toggle('change');
+        document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector("html").style.scrollBehavior = "unset";
+    }, 1000);
+    });
+});
+
+document.querySelector(".hamburger-menu").addEventListener("touch", () => {
+    document.querySelector(".container").classList.toggle("change");
+});
+
+document.querySelector(".scroll-btn").addEventListener ("touch", () => {
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector("html").style.scrollBehavior = "unset";
+    }, 1000);
+});
+
+document.querySelector(".button").addEventListener ("touch", () => {
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector("html").style.scrollBehavior = "unset";
+    }, 1000);
+});
+
+menu_item.forEach(item => {
+    item.addEventListener("touch" , () => {
         document.querySelector(".container").classList.toggle('change');
         document.querySelector("html").style.scrollBehavior = "smooth";
     setTimeout(() => {
