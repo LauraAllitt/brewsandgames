de59c654bf4c24b88eaa4aec81b5fa59921aebfe
Laura <laura.allitt@hotmail.com>
2020-10-08 18:33:40 +0100

    fixing navbar issue

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload = () => {
       document.querySelector("body").classList.add("display");
     }, 4000);
   };
-  
+
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("change");
 });
@@ -25,4 +25,4 @@ menu_item.forEach(item => {
         document.querySelector("html").style.scrollBehavior = "unset";
     }, 1000);
     });
-});
+}); 
